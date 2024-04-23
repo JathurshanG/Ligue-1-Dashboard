297cb6013ff6f30fd5651d13b6418881b9bbd383
JathurshanG <jathur93@gmail.com.com>
2024-04-23 22:45:11 +0200

    [000002 Update Color and add Titles for Graphs]

--- __main__.py
+++ __main__.py
@@ -14,8 +14,8 @@ df = pd.read_json('output/output.json').dropna(subset='awayScore')
 
 df['homeScore'] = df['homeScore'].astype(float)
 
-season = "2014-2015"
-team = "STADE DE REIMS"
+# season = "2014-2015"
+# team = "STADE DE REIMS"
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -28,14 +28,17 @@ app.layout = html.Div([
         )),
         dbc.Col(dcc.Dropdown(id="dropdown_Team"))
     ]),
+    
     dbc.Row([
         dbc.Col([dcc.Graph(id="graph1")],width=3),
         dbc.Col([dcc.Graph(id="graph2")],width=4),
         dbc.Col([dcc.Graph(id='graph3')],width=5)
     ]),
+    html.H1('Ligue1 Match Dashboard',style={"text-align":'center'}),
     dbc.Row([
         dbc.Col([dcc.Graph(id="graph4")],width=4),
-        dbc.Col(dash_table.DataTable(id='table1',style_table={'overflowX': 'auto'}),width=4,style={'margin': 'auto'}),
+        dbc.Col([html.H5('Last 5 Matchs',style={"text-align":"center"}),
+                 dash_table.DataTable(id='table1',style_table={'overflowX': 'auto','text-align':'center'})],width=4,style={'margin': 'auto'}),
         dbc.Col([dcc.Graph(id="graph5")],width=4)
 
     ])
@@ -75,13 +78,16 @@ def dtm(season,team):
     
     pieData = dtm.groupby(['vainqueur'],as_index=False).count()
     pieData = pieData[['vainqueur','id']].rename(columns={'id':'value'})
-    pieChart = px.pie(pieData,names='vainqueur',values='value').update_layout(showlegend=False,title_x=0.5).update_traces(textposition='inside', textinfo='percent+label')
+    pieChart = px.pie(pieData,names='vainqueur',values='value',hole=0.7,color_discrete_sequence=px.colors.qualitative.Pastel1,title="Team Result Breakdown")\
+                .update_layout(showlegend=False,title_x=0.5)\
+                .update_traces(textposition='inside', textinfo='percent+label')
     
     dtm.loc[dtm['homeTeam']==team,'domicile'] = 'Home'
     dtm.loc[dtm['homeTeam']!=team,'domicile'] = 'Away'
     barData = dtm.groupby(['vainqueur','domicile'],as_index=False).count()
-    barData = barData[['vainqueur','domicile','id']]
-    barChart = px.bar(barData,x='vainqueur',y='id',color="domicile").update_layout(showlegend=False,title_x=0.5)
+    barData = barData[['vainqueur','domicile','id']].rename(columns={"id":"Number of Match",'vainqueur':"Win/Lose/Draw"})
+    barChart = px.bar(barData,x='Win/Lose/Draw',y='Number of Match',color="domicile",title="Match Outcome Tally",color_discrete_sequence=[px.colors.qualitative.Pastel1[1],px.colors.qualitative.Pastel1[2]])\
+                 .update_layout(showlegend=False,title_x=0.5).update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
 
     dtm.loc[(dtm['homeTeam']==team) & (dtm['homeScore']>5),'goal'] = "Over 5"
     dtm.loc[(dtm['homeTeam']==team) & (dtm['goal'].isna()),'goal'] = dtm['homeScore']
@@ -89,13 +95,16 @@ def dtm(season,team):
     dtm.loc[(dtm['awayTeam']==team) & (dtm['goal'].isna()),'goal'] = dtm['awayScore']
     dtm['goal'] = dtm['goal'].astype(str)
     goalData = dtm.groupby(['goal'],as_index=False).count()
-    goalChart = px.bar(goalData,x='goal',y='id')
+    goalData = goalData[['goal','id']].rename(columns={"goal":"Number of Goal","id":'Number of Match'})
+    goalChart = px.bar(goalData,x='Number of Goal',y='Number of Match',title="Goals Scored Distribution",color_discrete_sequence=[px.colors.qualitative.Pastel1[2]])\
+                  .update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'}) 
 
     losingGame = dtm[dtm['vainqueur']=="lose"]
     loser = losingGame['homeTeam'].values.tolist() + losingGame['awayTeam'].values.tolist()
     loserDt= pd.DataFrame.from_dict(Counter(loser),orient="index").reset_index().rename(columns={'index':'names',0:'values'})
     loserDt = loserDt[loserDt['names']!=team].sort_values(by='values',ascending=False).head(5)
-    loseChart = px.bar(loserDt,x='names',y='values')
+    loseChart = px.bar(loserDt,x='names',y='values',title='Losses Against Teams',color_discrete_sequence=[px.colors.qualitative.Pastel1[0]])\
+                  .update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
 
     last5DaysResults = dtm.sort_values(by=['matchDay'],ascending=False).head(5)
     last5DaysResults['5 last Match'] = last5DaysResults['homeTeam'] + " " +last5DaysResults['homeScore'].astype(int).astype('str') + " - " + last5DaysResults['awayScore'].astype(int).astype('str') + " " +last5DaysResults['awayTeam']
@@ -120,9 +129,11 @@ def dtm(season,team):
     team_stats['goal_difference'] = team_stats['goals_for'] - team_stats['goals_against']
     team_stats.reset_index(inplace=True)
     team_stats.loc[team_stats['homeTeam']==team,'cluster'] = 3
-    ClusterTeams = px.scatter(team_stats, x='goals_for', y='goals_against', color='cluster',hover_name='homeTeam')
+    ClusterTeams = px.scatter(team_stats, x='goals_for', y='goals_against', color='cluster',hover_name='homeTeam',title="Goals Correlation Analysis",color_discrete_sequence=px.colors.qualitative.Pastel1)
     # Ajustement du style des marqueurs
-    ClusterTeams.update_layout(showlegend=False).update_traces(marker=dict(size=15, line=dict(width=2, color='DarkSlateGrey')))
+    ClusterTeams.update_layout(showlegend=False)\
+                .update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})\
+                .update_traces(marker=dict(size=15, line=dict(width=2)))
 
     return pieChart,barChart,goalChart,loseChart,tableRecom,ClusterTeams
 
